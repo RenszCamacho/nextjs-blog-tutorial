9b3af0769c8d17cb2a55b1b316f1ea60dcfda472
Renszo Camacho <varsovia84@gmail.com>
2021-02-04 16:38:04 +0100

    Layout Component and Css Modules.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 function FirstPost() {
   return (
-    <div>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to Home</a>
         </Link>
       </h2>
-    </div>
+    </Layout>
   );
 }
 
